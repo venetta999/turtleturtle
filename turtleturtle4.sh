11451b34d501ae5617ab81f44e3d5de60339fcca
venetta999 <66510220+venetta999@users.noreply.github.com>
2021-07-14 02:18:22 +0700

    Create turtleturtle4.sh

---
+++ turtleturtle4.sh
@@ -0,0 +1,3 @@
+! wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28/lolMiner_v1.28a_Lin64.tar.gz
+! tar -xf lolMiner_v1.28a_Lin64.tar.gz
+! ./1.28a/lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 -u ETH:0x0b9304ee5ec96dd935276c2f83cd7261ef058383.kt
