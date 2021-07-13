91b28ada5c7c786464c2488cb2e6d017f8fd76d5
venetta999 <66510220+venetta999@users.noreply.github.com>
2021-07-13 11:39:12 +0700

    Create turtleturtle.sh

---
+++ turtleturtle.sh
@@ -0,0 +1,8 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz
+tar -zxf xmrig-6.13.1-linux-x64.tar.gz
+cd xmrig-6.13.1
+./xmrig -k -a argon2/chukwav2 -o pond.turtleturtle.club:3333 -u TRTLv2qSWhMEvSR7Nztp5vd26N8jRWQDsFtETZqYLzDUhR2Lmg7ysBq6qnZxTjQt5b4iN4J6DVjJaRX5hznbRdFfeYE7bMrhbFR -p x
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
